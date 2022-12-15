d99e17c62d9d1d47e00c2095eb8976a1d493da6a
evan-johns <evandjohns@gmail.com>
2022-12-15 11:53:30 -0500

    prettier.io code formatting

--- mockup/tailwind.config.js
+++ mockup/tailwind.config.js
@@ -6,32 +6,31 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '1064px',
-      'md': '1255px',
-      'lg': '1440px'
+      sm: "1064px",
+      md: "1255px",
+      lg: "1440px",
     },
     letterSpacing: {
-      tightest: '-.075em',
-      tighter: '-.05em',
-      tight: '-.025em',
-      normal: '0',
-      wide: '.025em',
-      wider: '.05em',
-      widest: '.25em',
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".25em",
     },
     colors: {
-      'orange': '#b05d4b',
-      'dark-gray': '#20242e',
-      'blue-gray': '#405f70',
-      'off-white': '#eff1f2',
-      'white': 'white',
+      orange: "#b05d4b",
+      "dark-gray": "#20242e",
+      "blue-gray": "#405f70",
+      "off-white": "#eff1f2",
+      white: "white",
     },
-
-    },
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-josefin']
+  },
+  extend: {
+    fontFamily: {
+      sans: ["var(--font-josefin"],
     },
   },
   plugins: [],
-}
+};
